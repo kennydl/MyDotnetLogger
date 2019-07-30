e106ea10876b223419ee495ae41312966aef8860
Kenny Duy Luong <kennydl.dev@gmail.com>
2019-07-30 02:05:21 +0200

    Updated logger extensions

--- My.Dotnet.Logger/Extensions/LoggerExtensions.cs
+++ My.Dotnet.Logger/Extensions/LoggerExtensions.cs
@@ -22,72 +22,72 @@ namespace My.Dotnet.Logger.Extensions
             return logger;
         }
 
-        public static ILogger<T> LogInformation<T>(this ILogger<T> logger, object value, string message, params object[] args)
+        public static ILogger<T> LogInformation<T>(this ILogger<T> logger, string name, object value, string message, params object[] args)
         {
-            logger.AddProperty("property", value);
+            logger.AddProperty(name, value);
             logger.LogInformation(message, args);
             return logger;
         }
 
-        public static ILogger<T> LogInformation<T>(this ILogger<T> logger, object value, Exception exception, string message, params object[] args)
+        public static ILogger<T> LogInformation<T>(this ILogger<T> logger, string name, object value, Exception exception, string message, params object[] args)
         {
-            logger.AddProperty("property", value);
-            logger.LogInformation(exception: exception, message, args);
+            logger.AddProperty(name, value);
+            logger.LogInformation(exception, message, args);
             return logger;
         }
 
-        public static ILogger<T> LogDebug<T>(this ILogger<T> logger, object value, string message, params object[] args)
+        public static ILogger<T> LogDebug<T>(this ILogger<T> logger, string name, object value, string message, params object[] args)
         {
-            logger.AddProperty("property", value);
+            logger.AddProperty(name, value);
             logger.LogDebug(message, args);
             return logger;
         }
 
-        public static ILogger<T> LogDebug<T>(this ILogger<T> logger, object value, Exception exception, string message, params object[] args)
+        public static ILogger<T> LogDebug<T>(this ILogger<T> logger, string name, object value, Exception exception, string message, params object[] args)
         {
-            logger.AddProperty("property", value);
-            logger.LogDebug(exception: exception, message, args);
+            logger.AddProperty(name, value);
+            logger.LogDebug(exception, message, args);
             return logger;
         }
 
-        public static ILogger<T> LogWarning<T>(this ILogger<T> logger, object value, string message, params object[] args)
+        public static ILogger<T> LogWarning<T>(this ILogger<T> logger, string name, object value, string message, params object[] args)
         {
-            logger.AddProperty("property", value);
+            logger.AddProperty(name, value);
             logger.LogWarning(message, args);
             return logger;
         }
 
-        public static ILogger<T> LogWarning<T>(this ILogger<T> logger, object value, Exception exception, string message, params object[] args)
+        public static ILogger<T> LogWarning<T>(this ILogger<T> logger, string name, object value, Exception exception, string message, params object[] args)
         {
-            logger.AddProperty("property", value);
+            logger.AddProperty(name, value);
             logger.LogWarning(exception: exception, message, args);
             return logger;
         }
 
-        public static ILogger<T> LogError<T>(this ILogger<T> logger, object value, string message, params object[] args)
+        public static ILogger<T> LogError<T>(this ILogger<T> logger, string name, object value, string message, params object[] args)
         {
-            logger.AddProperty("property", value);
+            logger.AddProperty(name, value);
             logger.LogError(message, args);
             return logger;
         }
 
-        public static ILogger<T> LogError<T>(this ILogger<T> logger, object value, Exception exception, string message, params object[] args)
+        public static ILogger<T> LogError<T>(this ILogger<T> logger, string name, object value, Exception exception, string message, params object[] args)
         {
-            logger.AddProperty("property", value);
+            logger.AddProperty(name, value);
             logger.LogError(exception: exception, message, args);
             return logger;
         }
 
-        public static ILogger<T> LogCritical<T>(this ILogger<T> logger, object value, string message, params object[] args)
+        public static ILogger<T> LogCritical<T>(this ILogger<T> logger, string name, object value, string message, params object[] args)
         {
-            logger.AddProperty("property", value);
+            logger.AddProperty(name, value);
             logger.LogCritical(message, args);
             return logger;
         }
 
-        public static ILogger<T> LogCritical<T>(this ILogger<T> logger, object value, Exception exception, string message, params object[] args)
+        public static ILogger<T> LogCritical<T>(this ILogger<T> logger, string name, object value, Exception exception, string message, params object[] args)
         {
-            logger.AddProperty("property", value);
+            logger.AddProperty(name, value);
             logger.LogCritical(exception: exception, message, args);
             return logger;
         }
